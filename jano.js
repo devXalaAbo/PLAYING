e536ad0a1ebb6f364248896592b37b4ee7ef27df
Glitch (receptive-twilight-draw) <none>
2021-11-23 12:00:44 +0000

    Rewound to commit 0298c1d7213c5c850052d7384c4ce2ce40ceeffc + 0 edits

--- jano.js
+++ jano.js
@@ -1,60 +1,21 @@
 const Discord = require("discord.js");
-const smsm = new Discord.Client();
+const jano = new Discord.Client();
 //////// playing or dnd  streaming ////////
-smsm39smsm39.on("ready", () => {
-  console.log(
-    `Online In Servers`
-  );
-  let statuses = [
-  
-  `By : SMSM`,
-
-  ];
+jano.on("ready", () => {
+  console.log(`Online In Servers`);
+  let statuses = [`JUST ALLAH`];
   setInterval(function() {
     let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    smsm39.user.setActivity(STREAMING, {      
-    type: "playing", 
-    url: "https://www.twitch.tv/faith"
-    });
-  }, 2000);
-})smsm39.on("ready", () => {
-  console.log(
-    `Online In Servers`
-  );
-  let statuses = [
-  
-  `By : SMSM`,
-
-  ];
-  setInterval(function() {
-    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    smsm39.user.setActivity(STREAMING, {      
-    type: "playing", 
-    url: "https://www.twitch.tv/faith"
-    });
-  }, 2000);
-}.on("ready", () => {
-  console.log(
-    `Online In Servers`
-  );
-  let statuses = [
-  
-  `By : SMSM`,
-
-  ];
-  setInterval(function() {
-    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    smsm39.user.setActivity(STREAMING, {      
-    type: "playing", 
-    url: "https://www.twitch.tv/faith"
+    jano.user.setActivity(STREAMING, {
+      type: "STREAMING",
+      url: "https://www.twitch.tv/faith"
     });
   }, 2000);
 });
 ///////////////////////
-smsm.on("ready", () => {
-  var join = smsm.channels.get(""); /// id voice
+jano.on("ready", () => {
+  var join = jano.channels.get(""); /// id voice
   if (join) join.join();
 });
 //////////////// tokn lera da bne
-smsm.login(process.env.TOKEN);
-smsm.login(process.env.TOKEN2);
+jano.login("");
