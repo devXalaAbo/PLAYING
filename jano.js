212da8980ded726d3a0da159f4e9b7818eaa1f9a
Glitch (receptive-twilight-draw) <none>
2021-11-23 12:24:19 +0000

    🍉🚝 Checkpoint ./jano.js:33112061/6015

--- jano.js
+++ jano.js
@@ -1,24 +1,19 @@
 const Discord = require("discord.js");
-const jano = new Discord.Client();
+const client = new Discord.Client();
 //////// playing or dnd  streaming ////////
-jano.on("ready", () => {
-  console.log(`Online In Servers`);
-  let statuses = [`ldle`];
-  setInterval(function() {
-    let ldle = statuses[Math.floor(Math.random() * statuses.length)];
-    jano.user
-      .setActivity(ldle, {
-      
-      type: "PLAYING",
-      status: "ldle",
-      url: "https://www.twitch.tv/faith"
-    });
-  }, 2000);
+client.on('ready' , () =>{
+ console.log('Bot esta escuchando');
+ client.user.setStatus('dnd');
+
+ console.log('ldle', client.user.presence.status);
+
+
 });
+
 ///////////////////////
-jano.on("ready", () => {
-  var join = jano.channels.get(""); /// id voice
+client.on("ready", () => {
+  var join = client.channels.get(""); /// id voice
   if (join) join.join();
 });
 //////////////// tokn lera da bne
-jano.login(process.env.TOKEN);
+client.login(process.env.TOKEN);
