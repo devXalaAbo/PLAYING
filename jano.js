d5de48c46ba54e32c497f685fa0147db970c3356
Glitch (receptive-twilight-draw) <none>
2021-11-23 12:45:56 +0000

    😜🌋 Checkpoint ./jano.js:33112061/2036

--- jano.js
+++ jano.js
@@ -1,19 +1,18 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 //////// playing or dnd  streaming ////////
-client.on('ready' , () =>{
- console.log('Bot esta escuchando');
- client.user.setStatus('ldle');
 
- console.log('ldle', client.user.presence.status);
-
-
+client.on("ready", () =>{
+  console.log(`sleeping bot ${client.user.tag}!`);
+  client.user.setPresence({
+    status: "dnd",
+    game: {
+       name: "",
+       type: "PLAY",
+    }
+  });
 });
-
 ///////////////////////
-client.on("ready", () => {
-  var join = client.channels.get(""); /// id voice
-  if (join) join.join();
-});
+
 //////////////// tokn lera da bne
 client.login(process.env.TOKEN);
