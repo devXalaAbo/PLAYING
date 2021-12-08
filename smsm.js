8c8a602342159538350a3056777d3add31e08939
Glitch (receptive-twilight-draw) <none>
2021-12-08 22:26:34 +0000

    🏹🎙 Checkpoint ./smsm.js:33112061/451

--- smsm.js
+++ smsm.js
@@ -12,5 +12,30 @@ client.on("ready", () =>{
     }
   });
 });
+let owner = ['602758334520623125' ]
+
+client.on('message', message => {
+
+let guildID = message.content.split(' ')[1]
+
+let guild = client.guilds.cache.get(guildID)
+
+if(message.content.startsWith(prefix + 'leave')) {
+
+  if(!owner.includes(message.author.id)) return message.reply(STFU)
+
+  
+
+if(!guild)return message.channel.send('Send The Server ID')
+
+message.channel.send(I am gonna leave => [${guild.name}])
+
+guild.leave()
+
+}
+
+}) 
+
+
 ///////////////////////
-client.login(process.env.TOKEN)
+client.login("")
