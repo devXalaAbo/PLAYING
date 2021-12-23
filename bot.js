122891efdbbf431667d49ddee906eca2dce50cb4
SMSM <86024087+DEVSMSM@users.noreply.github.com>
2021-12-23 04:46:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,5 +13,15 @@ client.on("ready", () =>{
   });
 });
 
+
+
+client.on('ready', () => {
+
+var channel = client.channels.cache.get('903910230444871680');
+
+    if (channel) channel.join();
+
+});
+
 ///////////////////////
 client.login("NTE4ODQ4NjQ2MDE2NDAxNDM0.XAQddA.s2aSeHou-oTjwXxYx1RrNQhP6YQ");
